6cca10636b57b814b946e9c1696c3b1ae817499e
mashiourcse <mashiour.cseiiuc@gmail.com>
2024-05-30 10:39:33 +0600

    till 17:44

--- puppet.js
+++ puppet.js
@@ -1,23 +1,27 @@
 import puppeteer from "puppeteer";
+import {setTimeout} from "timers/promises";
 
 const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: {width: 1920, height: 1080},
     slowMo: 250,
     userDataDir: "temporary",
-
+    slowMo: 250
 });
 
 const page = await browser.newPage();
 
-await page.goto("https://devconfbd.com/", {
-  waitUntil: "networkidle2",
-  timeout: 60000,  
-});
+await page.goto("https://devconfbd.com/");
+let selector = "img[alt='guest']";
+await page.screenshot({ path: "devconfbd.png"});
+await page.waitForSelector(selector);
+await page.click(selector);
+await setTimeout(2000);
+await page.screenshot({ path: "guest.png"});
 
-await page.screenshot({ path: "devconfbd.png"})
-
-setTimeout( async()=>{
 
 await browser.close();
-}, 10000)
+// setTimeout( async()=>{
+
+// await browser.close();
+// }, 10000)
