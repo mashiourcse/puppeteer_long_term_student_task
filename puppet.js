cbc2931cfa2fff7ac65cead2044d5a356b18e42a
mashiourcse <mashiour.cseiiuc@gmail.com>
2024-05-30 11:54:44 +0600

    test done

--- puppet.js
+++ puppet.js
@@ -11,17 +11,15 @@ const browser = await puppeteer.launch({
 
 const page = await browser.newPage();
 
-await page.goto("https://devconfbd.com/");
-let selector = "img[alt='guest']";
-await page.screenshot({ path: "devconfbd.png"});
-await page.waitForSelector(selector);
-await page.click(selector);
-await setTimeout(2000);
-await page.screenshot({ path: "guest.png"});
+await page.goto('https://www.google.com/', {waitUntil: 'networkidle2'});
+await page.waitForSelector('[class="gLFyf"]')
+await page.type('[class="gLFyf"]', 'devconfbd')
+await page.click('[type="submit"]')
+await page.waitForSelector('[data-testid="result-title-a"]')
+await page.screenshot({ path: 'google_search.png'});
 
-
-await browser.close();
-// setTimeout( async()=>{
-
+// id="searchbox_input" #searchbox_input
+// [class="searchbox_searchButton__F5Bwq iconButton_button__6x_9C"]
+// [data-testid="result"] 
 // await browser.close();
 // }, 10000)
